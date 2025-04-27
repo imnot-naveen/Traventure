fae238cfaa84dd1723834d0caf9b94f1dd20e6d4
dimuthuh28 <dimuthuharshamal@gmail.com>
2025-04-27 18:50:46 +0530

    needs to be fixed

--- Server/includes/config.php
+++ Server/includes/config.php
@@ -2,7 +2,7 @@
 
     $db_user = 'root';
     $db_password = '';
-    $db_name = 'traventure4';
+    $db_name = 'traventure';
 
     $db = new PDO('mysql:host=127.0.0.1;dbname='.$db_name.';charset=utf8',$db_user,$db_password);
 
